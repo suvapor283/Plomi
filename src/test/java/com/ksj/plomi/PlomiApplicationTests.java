5a7c409c7b2cd27954cac7b4e4c2fde676edda04
suvapor283 <suvapor8632@gmail.com>
2025-12-08 19:36:50 +0900

    feat(domain): Define UserRole enum for user authorities

--- src/test/java/com/ksj/plomi/PlomiApplicationTests.java
+++ src/test/java/com/ksj/plomi/PlomiApplicationTests.java
@@ -1,31 +1,13 @@
 package com.ksj.plomi;
 
-import com.ksj.plomi.domain.test.TestEntity;
-import com.ksj.plomi.domain.test.TestRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PlomiApplicationTests {
 
-    @Autowired
-    private TestRepository testRepository;
-
     @Test
-    void saveAndFindTest() {
-        TestEntity test = new TestEntity();
-        test.setTitle("test title");
-        test.setContent("test content");
-
-        TestEntity saveTest = testRepository.save(test);
-        TestEntity foundTest = testRepository.findById(saveTest.getId()).orElse(null);
-
-        assertThat(foundTest).isNotNull();
-        assertThat(foundTest.getTitle()).isEqualTo("test title");
-        assertThat(foundTest.getContent()).isEqualTo("test content");
+    void contextLoads() {
     }
 
 }
