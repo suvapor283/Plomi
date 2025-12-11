05c57d323033e20f128873aa4dccc673b800c011
suvapor283 <suvapor8632@gmail.com>
2025-12-11 16:29:50 +0900

    refactor(auth): Remove redundant validation on password2 in SignupRequestDto

--- src/main/java/com/ksj/plomi/domain/auth/dto/SignupRequestDto.java
+++ src/main/java/com/ksj/plomi/domain/auth/dto/SignupRequestDto.java
@@ -26,8 +26,6 @@ public class SignupRequestDto {
     private String password;
 
     @NotBlank(message = "비밀번호 확인은 필수 입력 정보입니다.")
-    @Size(min = 8, max = 20, message = "비밀번호 확인은 8자 이상 20자 이하로 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$", message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
     private String password2;
 
     @Email(message = "유효하지 않은 이메일 형식입니다.")
